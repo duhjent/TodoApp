873f85a73287261275c55af41cfa8469ecf1af6c
Viktor Sabov <viktorszabo2002@gmail.com>
2021-01-28 16:15:08 +0200

    separated individual item login into new component

--- AngularClient/src/app/shared/todo.state.ts
+++ AngularClient/src/app/shared/todo.state.ts
@@ -62,7 +62,7 @@ export class TodoListState {
       tap(data => {
         const state = ctx.getState();
         let exceptChosen = state.todoList.filter(t => t.id !== action.item.id);
-        ctx.setState({todoList: [...exceptChosen, data]});
+        ctx.setState({...state, todoList: [...exceptChosen, data]});
       }),
       catchError(async (err) => {
         console.log(err);
@@ -70,6 +70,22 @@ export class TodoListState {
           duration: 1000
         })
       })
-    )
+    );
+  }
+
+  @Action(Todo.Delete)
+  deleteTodo(ctx: StateContext<TodoListStateModel>, action: Todo.Delete) {
+    return this.http.delete(`/api/todos/${action.id}`).pipe(
+      tap(data => {
+        const state = ctx.getState();
+        ctx.setState({...state, todoList: [...state.todoList.filter(t => t.id != action.id)]});
+      }),
+      catchError(async (err) => {
+        console.log(err);
+        this.snackBar.open('Error deleting todo', 'OK', {
+          duration: 1000
+        })
+      })
+    );
   }
 }
--- AngularClient/src/app/todo-item/todo-item.component.ts
+++ AngularClient/src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Todo } from '../shared/todo.actions';
+import { TodoItem } from '../todo-item';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() item: TodoItem;
+
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+  }
+
+  deleteItem() {
+    this.store.dispatch(new Todo.Delete(this.item.id));
+  }
+
+  updateItem() {
+    this.store.dispatch(new Todo.Edit(this.item));
   }
 
 }
