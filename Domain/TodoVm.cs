ee76510b427495dca901427df7c982e5370e8a76
Viktor Sabov <viktorszabo2002@gmail.com>
2021-02-04 23:09:50 +0200

    added validation on backend

--- Domain/TodoVm.cs
+++ Domain/TodoVm.cs
@@ -1,12 +1,27 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace TodoApp.Domain
 {
-    public class TodoVm
+    public class TodoVm : IValidatableObject
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public bool IsDone { get; set; }
         public List<string> Tags { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == null || Title.Trim() == "")
+            {
+                yield return new ValidationResult("Title must be specified");
+            }
+
+            if (Tags.Contains("") || Tags.Distinct().Count() != Tags.Count)
+            {
+                yield return new ValidationResult("Tags must be unique and not empty");
+            }
+        }
     }
 }
